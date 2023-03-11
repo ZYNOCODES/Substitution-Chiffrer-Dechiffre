f3492bf36166142bb9eca6b5d45665a56efa4d07
ZINE EDDINE <119061162+ZYNOCODES@users.noreply.github.com>
2023-03-12 00:18:11 +0100

    Exo2-Q1

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+#Chiffrement par Substitution
+
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 Pi = "GVCSALUTDPKZRNBIFXYEMWJQHO"
 text = "LA CRYPTANALYSE EST LA SCIENCE QUI CONSISTE A TENTER DE DECHIFFRER UN MESSAGE AYANT ETE CHIFFRE SANS POSSEDER " \
@@ -31,3 +34,32 @@ for letter in chiffrer:
 print("Text dechiffrer :")
 print(dechiffrer)
 
+#Chiffrement par decalage
+
+alphabet = list('abcdefghijklmnopqrstuvwxyz')
+alphabet2 = list('abcdefghijklmnopqrstuvwxyz')
+
+phrase = input("Ecrivez une phrase:")
+print(phrase)
+decalage = input("Valeur du decalage?")
+def ceasar():
+    x,y,z = 0,1,1
+    while x<26:
+        y = x+int(decalage)
+        z = y - 26
+        if(y<26):
+            alphabet[x] = alphabet[y]
+        else:
+            alphabet[x] = alphabet2[z]
+        x = x + 1
+    print(alphabet)
+ceasar()
+newPhraseList = list(phrase)
+nbLettres = len(phrase)
+newPhrase = ""
+n = 0
+while n<nbLettres:
+    newPhrase = newPhrase+""+newPhraseList[n]
+    phraseFinal = newPhrase.replace(newPhraseList[n],alphabet[n])
+    n = n + 1
+    print (phraseFinal)
